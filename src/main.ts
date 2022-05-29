f33b8882b36c9d494e7929103f36dc37d37ab327
rmq767 <2501706076@qq.com>
2022-05-29 14:02:29 +0800

    fork

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import pinia from '/@/stores/index';
+import App from './App.vue';
+import router from './router';
+import { directive } from '/@/utils/directive';
+import { i18n } from '/@/i18n/index';
+import other from '/@/utils/other';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import '/@/theme/index.scss';
+import mitt from 'mitt';
+
+const app = createApp(App);
+
+directive(app);
+other.elSvg(app);
+
+app.use(pinia).use(router).use(ElementPlus, { i18n: i18n.global.t }).use(i18n).mount('#app');
+
+app.config.globalProperties.mittBus = mitt();
